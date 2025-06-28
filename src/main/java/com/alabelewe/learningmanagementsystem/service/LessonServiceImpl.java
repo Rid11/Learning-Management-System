c91e681196eab7f686672f12a267a1c43f75cf39
ALABELEWE <oseinridwan5@gmail.com>
2025-06-28 18:37:48 +0100

    fixed bugs on dependencies on some of the beans of the appilication context form cycle

--- src/main/java/com/alabelewe/learningmanagementsystem/service/LessonServiceImpl.java
+++ src/main/java/com/alabelewe/learningmanagementsystem/service/LessonServiceImpl.java
@@ -1,47 +1,60 @@
 package com.alabelewe.learningmanagementsystem.service;
 
+import com.alabelewe.learningmanagementsystem.dao.LessonRepository;
 import com.alabelewe.learningmanagementsystem.entity.Lesson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LessonServiceImpl implements LessonService {
 
 
-    private LessonService lessonService;
+   private LessonRepository lessonRepository;
 
-    @Autowired
-    public LessonServiceImpl(LessonService lessonService) {
-        this.lessonService = lessonService;
-    }
+   @Autowired
+   public LessonServiceImpl(LessonRepository lessonRepository) {
+       this.lessonRepository = lessonRepository;
+   }
 
     public List<Lesson> findAll() {
-        return lessonService.findAll();
+        return lessonRepository.findAll();
     }
 
     public Lesson findById(int id) {
-        Lesson lesson = lessonService.findById(id);
+        Optional<Lesson> lesson = lessonRepository.findById(id);
 
-        if (lesson == null) {
-            throw new RuntimeException("Lesson not found" + id);
+        Lesson newLesson = null;
+
+        if (lesson.isPresent()){
+            newLesson = lesson.get();
+        }else{
+            throw new RuntimeException("Lesson not found");
         }
 
-        return lesson;
+        return newLesson;
+
     }
 
     public Lesson save(Lesson lesson) {
-        return lessonService.save(lesson);
+        return lessonRepository.save(lesson);
     }
 
     public void deleteById(int id) {
-        Lesson lesson = lessonService.findById(id);
-        if (lesson == null) {
-            throw new RuntimeException("Lesson not found" + id);
+
+        Optional<Lesson> lesson = lessonRepository.findById(id);
+
+        Lesson newLesson = null;
+
+        if (lesson.isPresent()){
+            lessonRepository.deleteById(id);
+        }else{
+            throw new RuntimeException("Lesson not found");
         }
 
-        lessonService.deleteById(id);
+
 
     }
 
